1023a840ac4d393516e61ba6824539ae069c20f7
Thomas De Grava <tdegrava@studiovisual.com.br>
2024-12-30 15:14:42 -0300

    update react and next

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,15 +9,6 @@ const config: Config = {
   ],
   theme: {
   	extend: {
-  		colors: {
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
   	}
   },
   plugins: [require("tailwindcss-animate")],
